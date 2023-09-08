dbd127f45f61a472342ddf53530f789b4ad4b4ef
joselin <a20206442@pucp.edu.pe>
2023-09-07 20:34:47 -0500

    proyecto base

---
+++ lab4_20206442/src/main/java/com/example/lab4_20206442/Lab420206442Application.java
@@ -0,0 +1,13 @@
+package com.example.lab4_20206442;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab420206442Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab420206442Application.class, args);
+    }
+
+}
---
+++ lab4_20206442/src/test/java/com/example/lab4_20206442/Lab420206442ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab4_20206442;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab420206442ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
